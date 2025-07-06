ac23a0b7b125c720acf6b71a00f950818bed997a
lkostoski <lkostoski@york.citycollege.eu>
2025-07-06 19:26:28 +0200

    add init function

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-board = [ ['_', '_', '_'],
-          ['_', '_', '_'],
-          ['_', '_', '_']];
+def init_board():
+    board = [ ['_', '_', '_'],
+              ['_', '_', '_'],
+              ['_', '_', '_'] ];
+    return board;
+
+board = init_board();
 
 def print_board(board):
     for row in board:
