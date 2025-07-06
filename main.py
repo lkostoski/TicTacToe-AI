37ccc281e4852ddf8f1c052d5dd7ac66f832bb60
lkostoski <lkostoski@york.citycollege.eu>
2025-07-06 19:40:23 +0200

    get move from player

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
 def init_board():
     board = [ ['_', '_', '_'],
               ['_', '_', '_'],
-              ['_', '_', '_'] ];
-    return board;
+              ['_', '_', '_'] ]
+    return board
 
-board = init_board();
+board = init_board()
 
 def print_board(board):
     for row in board:
-        print(row);
+        print(row)
 
-print_board(board);
+print_board(board)
+
+def get_move():
+    x = int(input("Enter row: "))
+    if x < 0 or x > 4:
+        print("Row number should be between 1 and 3")
+        return get_move();
+    y = int(input("Enter column: "))
+    if y < 0 or y > 4:
+        print("Column number should be between 1 and 3")
+        return get_move();
+    print(x,y)
+    return (x,y)
+
+get_move()
