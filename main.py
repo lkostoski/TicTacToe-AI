b75729e0bf218abafa4835851d9a30ba398b7a96
lkostoski <lkostoski@york.citycollege.eu>
2025-07-22 23:01:29 +0200

    add move to board

--- main.py
+++ main.py
@@ -12,16 +12,46 @@ def print_board(board):
 
 print_board(board)
 
+def is_valid_move(board, position):
+    x = position[0] - 1
+    y = position[1] - 1
+
+    if x < 0 or x > 2:
+        print("Row number should be between 1 and 3")
+        return False
+    
+    if y < 0 or y > 2:
+        print("Column number should be between 1 and 3")
+        return False
+    
+    if board[x][y] != '_':
+        print("Spot already taken")
+        return False
+    
+    return True
+
 def get_move():
     x = int(input("Enter row: "))
-    if x < 0 or x > 4:
-        print("Row number should be between 1 and 3")
-        return get_move();
     y = int(input("Enter column: "))
-    if y < 0 or y > 4:
-        print("Column number should be between 1 and 3")
-        return get_move();
-    print(x,y)
+
+    if not is_valid_move(board, (x,y)):
+        return get_move()
+    
     return (x,y)
 
-get_move()
+
+
+def player_turn():
+    player = 'X'
+
+
+# Unifnished, think differently, maybe its not like this
+def make_move(board, position, player):
+    x = position[0] - 1
+    y = position[1] - 1
+    new_board = board
+    new_board[x][y] = player
+    return new_board
+
+board = make_move(board, get_move(), 'X')
+print_board(board)
