09d50b6e2867be83c52312d9c6842bbecdca0b55
lkostoski <lkostoski@york.citycollege.eu>
2025-07-23 18:49:58 +0200

    finish initial tic tac toe game without AI

--- main.py
+++ main.py
@@ -4,13 +4,9 @@ def init_board():
               ['_', '_', '_'] ]
     return board
 
-board = init_board()
-
 def print_board(board):
     for row in board:
         print(row)
-
-print_board(board)
 
 def is_valid_move(board, position):
     x = position[0] - 1
@@ -30,28 +26,65 @@ def is_valid_move(board, position):
     
     return True
 
-def get_move():
+def get_move(board):
     x = int(input("Enter row: "))
     y = int(input("Enter column: "))
 
     if not is_valid_move(board, (x,y)):
-        return get_move()
+        return get_move(board)
     
     return (x,y)
 
-
-
-def player_turn():
-    player = 'X'
-
-
-# Unifnished, think differently, maybe its not like this
 def make_move(board, position, player):
     x = position[0] - 1
     y = position[1] - 1
-    new_board = board
-    new_board[x][y] = player
-    return new_board
+    board[x][y] = player
+    return board
 
-board = make_move(board, get_move(), 'X')
-print_board(board)
+def player_turn(turn):
+    if turn:
+        return 'X'
+    else: 
+        return 'O'
+
+def get_winner(board):
+    for row in board:
+        if row[0] == row[1] == row[2] != '_':
+            return row[0]
+    
+    for col in range(3):
+        if board[0][col] == board[1][col] == board[2][col] != '_':
+            return board[0][col]
+    
+    if board[0][0] == board[1][1] == board[2][2] != '_':
+        return board[0][0]
+    if board[0][2] == board[1][1] == board[2][0] != '_':
+        return board[0][2]
+    
+    return '_'
+
+def play():
+    turn = True
+    board = init_board()
+    print_board(board)
+    count = 0
+    is_winner = '_'
+
+    while is_winner == '_':
+        board = make_move(board, get_move(board), player_turn(turn))
+        count += 1
+        turn = not turn
+        print_board(board)
+
+        if count >= 5:
+            is_winner = get_winner(board)
+        
+        if count == 9:
+            break
+    
+    if not is_winner == '_':
+        print(f"The winner is {is_winner}")
+    else:
+        print ("The match has ended in a draw!")
+
+play()
