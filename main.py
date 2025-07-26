5d7dafd7168c2b468958854db6d07daa1cf4bc46
lkostoski <lkostoski@york.citycollege.eu>
2025-07-26 14:00:55 +0200

    add random ai

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+
 def init_board():
     board = [ ['_', '_', '_'],
               ['_', '_', '_'],
@@ -63,19 +65,38 @@ def get_winner(board):
     
     return '_'
 
+# AI
+
+def random_ai(board):
+    x = random.randrange(3) + 1
+    y = random.randrange(3) + 1
+    if not is_valid_move(board, (x,y)):
+        return random_ai(board)
+    
+    return (x,y)
+
+def random_ai2(board):
+    list_available = []
+    for x in range(3):
+        for y in range(3):
+            if board[x][y] == '_':
+                list_available.append((x+1,y+1))
+    return random.choice(list_available)
+
 def play():
     turn = True
     board = init_board()
     print_board(board)
+    print()
     count = 0
     is_winner = '_'
 
     while is_winner == '_':
-        board = make_move(board, get_move(board), player_turn(turn))
+        board = make_move(board, random_ai2(board), player_turn(turn))
         count += 1
         turn = not turn
         print_board(board)
-
+        print()
         if count >= 5:
             is_winner = get_winner(board)
         
