01d55d7d4a9e27bf4573964898fdb04fb3c59565
lkostoski <lkostoski@york.citycollege.eu>
2025-07-26 17:21:38 +0200

    test ais against eachother

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 import random
 
 def init_board():
@@ -67,7 +68,7 @@ def get_winner(board):
 
 # AI
 
-def random_ai(board):
+def random_ai(board, player):
     x = random.randrange(3) + 1
     y = random.randrange(3) + 1
     if not is_valid_move(board, (x,y)):
@@ -75,7 +76,7 @@ def random_ai(board):
     
     return (x,y)
 
-def random_ai2(board):
+def random_ai2(board, player):
     list_available = []
     for x in range(3):
         for y in range(3):
@@ -125,7 +126,7 @@ def finds_winning_and_losing_moves_ai(board, player):
 
     return random.choice(list_available)
 
-def play():
+def play_vs_ai():
     turn = random.choice([True, False])
     board = init_board()
     print_board(board)
@@ -133,7 +134,7 @@ def play():
     count = 0
     is_winner = '_'
 
-    while is_winner == '_':
+    while is_winner == '_' and count < 9:
         current_player = player_turn(turn)
         if current_player == 'O':
             board = make_move(board, finds_winning_and_losing_moves_ai(board, current_player), current_player)
@@ -145,13 +146,80 @@ def play():
         print()
         if count >= 5:
             is_winner = get_winner(board)
-        
-        if count == 9:
-            break
     
     if not is_winner == '_':
         print(f"The winner is {is_winner}")
     else:
         print ("The match has ended in a draw!")
 
-play()
+def select_ai(ai_name, board, player):
+    if ai_name == 'random_ai':
+        return random_ai(board, player)
+    elif ai_name == 'random_ai2':
+        return random_ai2(board, player)
+    elif ai_name == 'finds_winning_moves_ai':
+        return finds_winning_moves_ai(board, player)
+    elif ai_name == 'finds_winning_and_losing_moves_ai':
+        return finds_winning_and_losing_moves_ai(board, player)
+    elif ai_name == 'human':
+        return make_move(board, player)
+    else:
+        print(f"Unknown AI: {ai_name}")
+        return random_ai2(board, player)
+    
+def play_with_ais(x_ai_name, y_ai_name):
+    turn = random.choice([True, False])
+    board = init_board()
+    print_board(board)
+    print()
+    count = 0
+    is_winner = '_'
+
+    while is_winner == '_' and count < 9:
+        current_player = player_turn(turn)
+        if current_player == 'O':
+            board = make_move(board, select_ai(x_ai_name, board, current_player), current_player)
+        else:
+            board = make_move(board, select_ai(y_ai_name, board, current_player), current_player)
+        count += 1
+        turn = not turn
+        print_board(board)
+        print()
+        if count >= 5:
+            is_winner = get_winner(board)
+    
+    if not is_winner == '_':
+        print(f"The winner is {is_winner}")
+        if is_winner == 'O': return 1
+        else: return 2
+    else:
+        print ("The match has ended in a draw!")
+        return 0
+
+def repeated_play(x_ai_name, y_ai_name):
+    count = 0
+    draws = 0
+    x = 0
+    y = 0
+    while count < 10000:
+        winner = play_with_ais(x_ai_name, y_ai_name)
+        if winner == 0: draws += 1
+        elif winner == 1: x += 1
+        elif winner == 2: y += 1
+        count += 1
+    print (f"{x_ai_name} won {x} times.")
+    print (f"{y_ai_name} won {y} times.")
+    print (f"{draws} draws.")
+    print()
+    print (f"{x_ai_name} won {(x/count)*100} %")
+    print (f"{y_ai_name} won {(y/count)*100} %")
+    print (f"They drew {(draws/count)*100} %")
+
+if __name__ == "__main__":
+    if len(sys.argv) == 3:
+        x_ai = sys.argv[1]
+        o_ai = sys.argv[2]
+        repeated_play(x_ai, o_ai)
+    else:
+        print("Starting default game: random_ai2 vs finds_winning_and_losing_moves_ai")
+        play_with_ais('random_ai2', 'finds_winning_and_losing_moves_ai')
